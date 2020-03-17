2d5a5157e2181bcec6674da157bc8afddbd8c482
Rory <rollin_ror@hotmail.com>
2020-03-17 23:34:45 +0000

    Add project files.

---
+++ XMLDeserialisation/Department.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace XMLDeserialisation
+{
+    public class Department
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int RoomNumber { get; set; }
+        public decimal Budget { get; set; }
+
+        [XmlArrayItem("Employee")]
+        public List<string> Employees { get; set; }
+    }
+}
---
+++ XMLDeserialisation/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace XMLDeserialisation
+{
+    class Program
+    {
+        static string xmlFilePath = string.Empty;
+
+        static void Main(string[] args)
+        {
+            Initialise();
+
+            DeserializeObject(xmlFilePath);
+
+            Console.ReadKey();
+        }
+
+        static void Initialise()
+        {
+            xmlFilePath = ConfigurationManager.AppSettings["XMLFilePath"];
+        }
+
+        static void DeserializeObject(string filename)
+        {
+            // Create an instance of the XmlSerializer.
+            var serializer = new XmlSerializer(typeof(Department));
+
+            // Declare an object variable of the type to be deserialized.
+            Department department;
+
+            using (Stream reader = new FileStream(filename, FileMode.Open))
+            {
+                // Call the Deserialize method to restore the object's state.
+                department = (Department)serializer.Deserialize(reader);
+            }
+
+            // Write out the properties of the object.
+            Console.WriteLine(department.Name);
+            Console.WriteLine(department.Description);
+            Console.WriteLine(department.RoomNumber);
+            Console.WriteLine(department.Budget);
+
+            Console.WriteLine("Employees:");
+
+            foreach (var employee in department.Employees)
+                Console.WriteLine(employee);
+        }
+    }
+}
